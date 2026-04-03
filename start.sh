b258a112859b8ad2fd9cb590ed01516208ac2fd2
DaniOnFire <danielmancaniellogg@gmail.com>
2026-04-03 18:20:09 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# Aggiorna pip e installa Playwright
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-# Installa Chromium per Playwright
+python3 -m pip install playwright
 python3 -m playwright install chromium
-
-# Avvia il bot
 python3 monitor.py
